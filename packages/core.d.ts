eb953476fbeb45cecf42364585c8a6878680b04f
aziznal <aziznal.dev@gmail.com>
2022-07-17 14:49:54 +0300

    Remove previous core build artifacts

--- packages/core.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "src/counter/counter-repository.interface" {
-    export abstract class CounterRepository {
-        abstract getCounter(): Promise<number>;
-        abstract setCounter(newCounter: number): void;
-    }
-}
-//# sourceMappingURL=core.d.ts.map
