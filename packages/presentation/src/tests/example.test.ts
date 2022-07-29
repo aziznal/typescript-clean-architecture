73f41ecd8f330b5ecd78b1e77eca279548b8687d
aziznal <aziznal.dev@gmail.com>
2022-07-29 20:25:12 +0300

    Update package-lock files in all packages

--- packages/presentation/src/tests/example.test.ts
+++ packages/presentation/src/tests/example.test.ts
@@ -1,11 +1,5 @@
-import { Foo } from '../app/example/foo';
-
 describe('Example Test', () => {
     it('Adds two and two together', () => {
         expect(2 + 2).toBe(4);
-    });
-
-    it('Imports things', () => {
-        expect(Foo).toBe(3);
     });
 });
